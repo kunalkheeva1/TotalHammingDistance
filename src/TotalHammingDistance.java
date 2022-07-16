233751701802dc7f1f630c32c8dedad6e54aa2c3
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-15 20:14:13 -0700

    added comments to better understand the properties of the total hamming distance

--- src/TotalHammingDistance.java
+++ src/TotalHammingDistance.java
@@ -2,16 +2,22 @@ public class TotalHammingDistance {
 
 
     public static int totalHammingDistance(int[] arr){
-
+        // get a sum to return the count at the end
         int sum = 0;
+
+        // integer cannot have more than 32 bits
         for(int i=0; i<32; i++){
+            // count the 1 bits, of every element at particular ith index
             int count =0;
             for(int j=0; j<arr.length; j++){
                 if(((arr[j]) & (1<<i)) >0){
                    count ++;
                 }
+                // after getting the count, multiply it will the remaining 0th bit elements to count the pairs
+                // and repeat it with the other ith indexes of same elements of the array
             }sum += count*(arr.length-count);
-        }return sum;
+        }// return the sum at the end
+        return sum;
     }
 
 
