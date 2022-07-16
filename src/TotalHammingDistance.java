52c42d895ae5ed7c100dbb9e3e97435ac771c3be
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-15 20:10:42 -0700

    created a method to count the total hamming distance between the pairs of given elements in an array

--- src/TotalHammingDistance.java
+++ src/TotalHammingDistance.java
@@ -1,3 +1,21 @@
 public class TotalHammingDistance {
-    ps
+
+
+    public static int totalHammingDistance(int[] arr){
+
+        int sum = 0;
+        for(int i=0; i<32; i++){
+            int count =0;
+            for(int j=0; j<arr.length; j++){
+                if(((arr[j]) & (1<<i)) >0){
+                   count ++;
+                }
+            }sum += count*(arr.length-count);
+        }return sum;
+    }
+
+
+    public static void main(String[] args) {
+
+    }
 }
